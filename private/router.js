14daab60f0a6159d8f1b58b29cdcd161d4b87897
Pedro Elias <epedro520@gmail.com>
2022-07-20 21:59:41 -0300

    upgrade: updating the router to acess the controller

--- private/router.js
+++ private/router.js
@@ -2,8 +2,12 @@ const Router = require("electron-router")
 const Controller =  require('./controller/controller')
 let router = Router('MAIN')
 
-router.post('create_user', (req, res)=>{Controller.addUser})
-router.post('create_product', (req, res)=>{Controller.addProduct})
-
+router.post('createUser', (req, res)=>{Controller.addUser})
+router.post('createProduct', (req, res)=>{Controller.addProduct})
+router.post('createServices', (req, res)=>{Controller.addServices})
+router.post('deleteUser', (req, res)=>{Controller.deleteUser})
+router.post('getUser', (req, res)=>{Controller.getUser})
+router.post('getAllUser', (req, res)=>{Controller.getAllUsers})
+router.post('updateUser', (req, res)=>{Controller.updateUser})
 
 
